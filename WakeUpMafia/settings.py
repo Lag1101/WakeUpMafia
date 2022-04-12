da5a908b879716af9ade5448c29d56601224e515
Vasily <LomanovVasiliy@gmail.com>
2022-04-12 23:52:38 +0300

    BASE_DIR

--- WakeUpMafia/settings.py
+++ WakeUpMafia/settings.py
@@ -122,17 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__).parent.parent)
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'Mafia/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/Mafia/static')
 STATIC_URL = '/Mafia/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'Mafia/static'),
 )
 
 # Default primary key field type
