7c100d93225e8fcaeff1ec0862bd713457e099da
Vasily <LomanovVasiliy@gmail.com>
2022-04-12 23:34:49 +0300

    BASE_DIR = './'

--- WakeUpMafia/settings.py
+++ WakeUpMafia/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = './'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
