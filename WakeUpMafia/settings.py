527917b040c492e61000609bfafa29143d74ff9f
Vasily <LomanovVasiliy@gmail.com>
2022-04-12 23:53:43 +0300

    slash

--- WakeUpMafia/settings.py
+++ WakeUpMafia/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '/Mafia/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Mafia/static')
 STATIC_URL = '/Mafia/static/'
 
 # Extra places for collectstatic to find static files.
