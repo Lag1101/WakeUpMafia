b47677a1d59ca2eb167eccf5753e059764092194
Vasily <LomanovVasiliy@gmail.com>
2022-04-13 00:05:37 +0300

    whitenoise

--- WakeUpMafia/wsgi.py
+++ WakeUpMafia/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
 
