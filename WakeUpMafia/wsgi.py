8b5badfb3dea3ecf2fe0415194b3e6f725b2185c
Vasily <LomanovVasiliy@gmail.com>
2022-04-13 00:33:39 +0300

    STATICFILES_DIRS

--- WakeUpMafia/wsgi.py
+++ WakeUpMafia/wsgi.py
@@ -16,4 +16,4 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WakeUpMafia.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application = WhiteNoise(application, root=settings.STATICFILES_DIRS[0])
