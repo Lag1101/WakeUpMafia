7569b928455d043e684803da2813001083c71fa3
Vasily <LomanovVasiliy@gmail.com>
2022-04-12 23:18:28 +0300

    ext db

--- WakeUpMafia/settings.py
+++ WakeUpMafia/settings.py
@@ -14,6 +14,10 @@ import django_heroku
 from pathlib import Path
 import os
 
+import environ
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,10 +82,7 @@ WSGI_APPLICATION = 'WakeUpMafia.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
