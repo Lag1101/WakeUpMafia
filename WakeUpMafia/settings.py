40cc23c7d6d846504c2071165c2cd01d838c3eda
Vasily <LomanovVasiliy@gmail.com>
2022-04-18 23:08:14 +0300

    ALLOWED_HOSTS

--- WakeUpMafia/settings.py
+++ WakeUpMafia/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure-i0)@)3%-ny(@b5^3w+)+687-(omn5_5us5d75^+ci@df0oyjiu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["51.250.105.245"]
 
 
 # Application definition
